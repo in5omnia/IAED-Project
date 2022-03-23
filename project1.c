58b9e854e0fe1d205344b1bd34b06249150769c4
in5omnia <93283175+in5omnia@users.noreply.github.com>
2022-03-23 09:52:00 +0000

    struct date and struct time done, need to see if I should ( and how to) associate them, plus write, sum, etc

--- project1.c
+++ project1.c
@@ -1,5 +1,8 @@
 /* Projeto1.c */
-#include <stdio.h>; <stdlib.h>; <ctype.h>; <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /* cant use qsort() */
 
 
@@ -23,6 +26,45 @@ Date
 Time?
  */
 
+typedef struct {
+    int day;
+    int month;
+    int year;
+} date;
+
+int check_date(date Date){
+    int Months30days = { 4, 6, 9, 11 }, Months31days = { 1, 3, 5, 7, 8, 10, 12 };
+    if (Date.month == 2 && Date.day > 28)       /* caso dos 28 dias de fevereiro */
+        return 0;
+    for (i = 0, i < 7, i++){
+        if (i < 4 && Date.month == Months30days[i] && Date.day > 30) /* SERIA MAIS EFICIENTE ESTAR NOUTRO FOR?*/
+            return 0;
+        if (Date.month == Months31days[i] && Date.day > 31)
+            return 0;
+    }
+    // falta ver se data for antes ou mais de 1 ano no futuro
+    return 1;
+}
+
+date sum_dates(date Date1, date Date2){
+    date DateSum;
+    DateSum.hour = Date1.day + Date1.day
+}
+
+
+typedef struct {
+    int hour;
+    int min;
+} time;
+
+date sum_times(time Time1, time Time2){
+    time TimeSum;       // MAS TENHO DE TER EM CONTA QUE PODE MUDAR DE DIA LOGO NAO POSSO INSERIR SO TIMES, TEM DE SER UMA OUTRA STRUCT MAYBE
+    TimeSum.hour = Time1.hour + Time1.hour
+}
+
+
+
+
 typedef struct airport Airport;
 Airport listAirports();
 Airport addAirport();
@@ -33,6 +75,7 @@ Flight list_Departure_Flights();
 Flight addFlight(Flight f);
 typedef struct date Date;
 Date nextDay(Date today);
+
 
 
 int main(){
